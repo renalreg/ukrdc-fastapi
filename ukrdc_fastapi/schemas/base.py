afdd1a0ac219cf554f3516ad85d14c590bfac14c
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-02-12 13:07:44 +0000

    Added model class for validating lazy SQLAlchemy relationships

--- ukrdc_fastapi/schemas/base.py
+++ ukrdc_fastapi/schemas/base.py
@@ -1,6 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from sqlalchemy.orm import Query
 
 
 class ORMModel(BaseModel):
     class Config:
         orm_mode = True
+
+    @validator("*", pre=True)
+    def evaluate_lazy_columns(cls, v):
+        if isinstance(v, Query):
+            return v.all()
+        return v
+
+
+class LazyORMModel(ORMModel):
+    @validator("*", pre=True)
+    def evaluate_lazy_columns(cls, v):
+        if isinstance(v, Query):
+            return v.all()
+        return v
