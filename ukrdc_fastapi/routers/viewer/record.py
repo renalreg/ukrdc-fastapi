d92a26403a6e2c5cbaf02466c12d26c1e452736e
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-02-12 16:57:09 +0000

    Raise 404 if record is missing

--- ukrdc_fastapi/routers/viewer/record.py
+++ ukrdc_fastapi/routers/viewer/record.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from ukrdc_fastapi.dependencies import get_ukrdc3
@@ -10,5 +10,7 @@ router = APIRouter()
 
 @router.get("/{pid}", response_model=PatientRecordSchema)
 def patient_records(pid: str, ukrdc3: Session = Depends(get_ukrdc3)):
-    response = ukrdc3.query(PatientRecord).filter(PatientRecord.pid == pid).first()
-    return response
+    record = ukrdc3.query(PatientRecord).filter(PatientRecord.pid == pid).first()
+    if not record:
+        raise HTTPException(404, detail="Record not found")
+    return record
