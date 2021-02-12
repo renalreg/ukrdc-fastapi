234defa6dcb733d247d677e9d5719577e1c849b7
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-02-12 16:36:22 +0000

    Cleaned up DB session dependencies

--- ukrdc_fastapi/dependencies.py
+++ ukrdc_fastapi/dependencies.py
@@ -1,17 +1,31 @@
+from typing import Generator
+
+from sqlalchemy.orm import Session
+
 from .database import JtraceSession, Ukrdc3Session
 
 
-def get_ukrdc3():
-    db = Ukrdc3Session()
+def get_ukrdc3() -> Generator[Session, None, None]:
+    """Yeild a new UKRDC3 database session
+
+    Yields:
+        [Session]: UKRDC3 database session
+    """
+    session = Ukrdc3Session()
     try:
-        yield db
+        yield session
     finally:
-        db.close()
+        session.close()
 
 
-def get_jtrace():
-    db = JtraceSession()
+def get_jtrace() -> Generator[Session, None, None]:
+    """Yeild a new JTRACE database session
+
+    Yields:
+        Generator[Session]: JTRACE database session
+    """
+    session = JtraceSession()
     try:
-        yield db
+        yield session
     finally:
-        db.close()
+        session.close()
