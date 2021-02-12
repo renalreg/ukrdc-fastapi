e40fbc84475bd73247362fb78dbe75e1138b8085
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-02-12 16:36:58 +0000

    Removed unused demo root view

--- ukrdc_fastapi/main.py
+++ ukrdc_fastapi/main.py
@@ -5,9 +5,4 @@ from ukrdc_fastapi.routers import viewer
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"msg": "Hello World"}
-
-
 app.include_router(viewer.router, prefix="/viewer")
