5981d53c5a367d5aa916397384ecbe9b58d60913
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-02-15 17:17:24 +0000

    Added post_mirth_message

--- ukrdc_fastapi/utils.py
+++ ukrdc_fastapi/utils.py
@@ -1,5 +1,9 @@
 from typing import Optional
 
+import requests
+
+from .config import settings
+
 
 def build_db_uri(
     driver: str,
@@ -25,3 +29,14 @@ def build_db_uri(
     if driver == "sqlite":
         return f"{driver}:///{name}"
     return f"{driver}://{user}:{password}@{host}:{port}/{name}"
+
+
+def post_mirth_message(mirth_path, message):
+    """Post message to the Mirth HTTP listener."""
+    mirth_path_clean = mirth_path.strip("/")
+    mirth_url = settings.mirth_url
+
+    url = f"{mirth_url}/{mirth_path_clean}/"
+    headers = {"content-type": "application/xml"}
+
+    return requests.post(url, data=message, headers=headers)
