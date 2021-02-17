9e906f22a9905f5efcddf59fcadc1ae24a757caa
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-02-17 17:34:59 +0000

    Added documentation tags and root redirect

--- ukrdc_fastapi/main.py
+++ ukrdc_fastapi/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
 from ukrdc_fastapi.routers import laborders, records, workitems
 
-app = FastAPI()
+app = FastAPI(
+    title="UKRDC API v2",
+    description="Early test version of an updated, simpler UKRDC API",
+    version="0.0.0",
+)
 
 
-app.include_router(laborders.router, prefix="/laborders")
-app.include_router(records.router, prefix="/records")
-app.include_router(workitems.router, prefix="/workitems")
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/docs")
+
+
+app.include_router(laborders.router, prefix="/laborders", tags=["Lab Orders"])
+app.include_router(records.router, prefix="/records", tags=["Patient Records"])
+app.include_router(workitems.router, prefix="/workitems", tags=["Work Items"])
 
 
 if __name__ == "__main__":
