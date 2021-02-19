098c6413d3d610ce1bbf289c0db9c36c398745ef
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-02-19 14:04:09 +0000

    Bind to localhost in __main__

--- ukrdc_fastapi/main.py
+++ ukrdc_fastapi/main.py
@@ -26,4 +26,4 @@ app.include_router(workitems.router, prefix="/workitems", tags=["Work Items"])
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
