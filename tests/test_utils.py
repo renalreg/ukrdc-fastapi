bec0a351f71f969927de5a4faa459bf703bdcee0
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-02-19 12:59:21 +0000

    Added basic util tests

---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+from ukrdc_fastapi import utils
+
+
+def test_build_db_uri_general():
+    assert (
+        utils.build_db_uri("postgres", "host", 5432, "user", "pass", "dbname")
+        == "postgres://user:pass@host:5432/dbname"
+    )
+
+
+def test_build_db_uri_sqlite():
+    assert (
+        utils.build_db_uri("sqlite", "host", 5432, "user", "pass", "dbname")
+        == "sqlite:///dbname"
+    )
