bb998fda3e3a5779125bcc23f22ed1ea3c7490f3
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-02-24 12:25:52 +0000

    Added LabOrder NI filtering

--- tests/test_laborders.py
+++ tests/test_laborders.py
@@ -1,24 +1,70 @@
 from datetime import datetime
 
-from ukrdc_fastapi.models.ukrdc import LabOrder, ResultItem
+from ukrdc_fastapi.models.ukrdc import (
+    LabOrder,
+    PatientNumber,
+    PatientRecord,
+    ResultItem,
+)
 
 
 def test_laborders_list(client):
     response = client.get("/laborders")
     assert response.status_code == 200
-    assert response.json() == {
-        "items": [
-            {
-                "id": "LABORDER1",
-                "entered_at_description": None,
-                "entered_at": None,
-                "specimen_collected_time": "2020-03-16T00:00:00",
-            }
-        ],
-        "total": 1,
-        "page": 0,
-        "size": 50,
-    }
+    assert response.json()["items"] == [
+        {
+            "id": "LABORDER1",
+            "entered_at_description": None,
+            "entered_at": None,
+            "specimen_collected_time": "2020-03-16T00:00:00",
+        }
+    ]
+
+
+def test_laborders_list_filtered_ni(ukrdc3_session, client):
+    patient_record = PatientRecord(
+        pid="PYTEST01:LABORDERS:00000000L",
+        sendingfacility="PATIENT_RECORD_SENDING_FACILITY_1",
+        sendingextract="PV",
+        localpatientid="00000000L",
+        ukrdcid="000000001",
+        lastupdated=datetime(2020, 3, 16),
+        repository_creation_date=datetime(2020, 3, 16),
+    )
+    patient_number = PatientNumber(
+        id=2,
+        pid="PYTEST01:LABORDERS:00000000L",
+        number="111111111",
+        organization="NHS",
+        numbertype="NI",
+    )
+    laborder = LabOrder(
+        id="LABORDER_TEST1_1",
+        pid="PYTEST01:LABORDERS:00000000L",
+        external_id="EXTERNAL_ID_TEST1_1",
+        order_category="ORDER_CATEGORY_TEST1_1",
+        specimen_collected_time=datetime(2020, 3, 16),
+    )
+    ukrdc3_session.add(patient_record)
+    ukrdc3_session.add(patient_number)
+    ukrdc3_session.add(laborder)
+    ukrdc3_session.commit()
+
+    # Check we have multiple laborders when unfiltered
+    response_unfiltered = client.get("/laborders")
+    assert len(response_unfiltered.json()["items"]) == 2
+
+    # Filter by NI
+    response = client.get("/laborders?ni=111111111")
+    assert response.status_code == 200
+    assert response.json()["items"] == [
+        {
+            "id": "LABORDER_TEST1_1",
+            "entered_at_description": None,
+            "entered_at": None,
+            "specimen_collected_time": "2020-03-16T00:00:00",
+        }
+    ]
 
 
 def test_laborder(client):
