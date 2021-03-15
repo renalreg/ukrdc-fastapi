6b254649c9af21f794e1b1afa1c8ff040f3d8ca1
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-03-15 16:25:10 +0000

    Raise more sensible DB error messages

--- ukrdc_fastapi/dependencies.py
+++ ukrdc_fastapi/dependencies.py
@@ -1,10 +1,16 @@
 from typing import Generator
 
+from fastapi import HTTPException
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import Session
 
 from .config import settings
 from .database import ErrorsSession, JtraceSession, Ukrdc3Session
 from .mirth import MirthConnection
+
+JTRACE_FRIENDLY_ERROR_CODES = {"e3q8": "Error connecting to JTRACE database."}
+UKRDC3_FRIENDLY_ERROR_CODES = {"e3q8": "Error connecting to UKRDC3 database."}
+ERRORS_FRIENDLY_ERROR_CODES = {"e3q8": "Error connecting to ERRORSDB database."}
 
 
 def get_ukrdc3() -> Generator[Session, None, None]:
@@ -16,6 +22,11 @@ def get_ukrdc3() -> Generator[Session, None, None]:
     session = Ukrdc3Session()
     try:
         yield session
+    except OperationalError as e:
+        raise HTTPException(
+            500,
+            detail=UKRDC3_FRIENDLY_ERROR_CODES.get(e.code, "UKRDC3 Operational Error"),
+        ) from e
     finally:
         session.close()
 
@@ -29,6 +40,11 @@ def get_jtrace() -> Generator[Session, None, None]:
     session = JtraceSession()
     try:
         yield session
+    except OperationalError as e:
+        raise HTTPException(
+            500,
+            detail=JTRACE_FRIENDLY_ERROR_CODES.get(e.code, "JTRACE Operational Error"),
+        ) from e
     finally:
         session.close()
 
@@ -42,6 +58,13 @@ def get_errorsdb() -> Generator[Session, None, None]:
     session = ErrorsSession()
     try:
         yield session
+    except OperationalError as e:
+        raise HTTPException(
+            500,
+            detail=ERRORS_FRIENDLY_ERROR_CODES.get(
+                e.code, "ERRORSDB Operational Error"
+            ),
+        ) from e
     finally:
         session.close()
 
