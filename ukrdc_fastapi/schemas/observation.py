4abb89c5b76876d3f2652ec9bb7b90e66eb0f1be
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-04-14 15:28:53 +0100

    Made observation_desc and  observation_units optional. Fixes UI-32

--- ukrdc_fastapi/schemas/observation.py
+++ ukrdc_fastapi/schemas/observation.py
@@ -6,9 +6,9 @@ from .base import OrmModel
 
 class ObservationSchema(OrmModel):
     observation_time: datetime.datetime
-    observation_desc: str
+    observation_desc: Optional[str]
     observation_value: str
-    observation_units: str
+    observation_units: Optional[str]
     pre_post: Optional[str]
     entered_at: Optional[str]
     entered_at_description: Optional[str]
