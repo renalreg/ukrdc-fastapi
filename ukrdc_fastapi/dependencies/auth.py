28d08bcb04313dd3e1d8bed8e1d6845a5dbec6d4
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-04-26 17:50:54 +0100

    Migrated auth settings to Pydantic config

--- ukrdc_fastapi/dependencies/auth.py
+++ ukrdc_fastapi/dependencies/auth.py
@@ -1,3 +1,5 @@
+from ukrdc_fastapi.config import settings
+
 from .okta import OktaAccessToken, OktaAuth
 from .okta import OktaUserModel as User
 
@@ -8,14 +10,10 @@ class UKRDCAccessToken(OktaAccessToken):
     ukrdc: list[str]
 
 
-# TODO: Migrate args to dotenv
 auth = OktaAuth(
-    "https://dev-58161221.okta.com/oauth2/ausn7fa9zfh1DC2La5d6",
-    "api://ukrdc",
-    [
-        "0oan75eooLX2DcdQK5d6",  # Swagger client ID
-        "0oan98slw3m4mnhxq5d6",  # Nuxt client ID
-    ],
+    settings.oauth_issuer,
+    settings.oauth_audience,
+    [settings.app_client_id, settings.swagger_client_id],
     token_model=UKRDCAccessToken,
     permission_key="ukrdc",
 )
