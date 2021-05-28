870ea0087ee8370937c385db0c5b009fdc627e67
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-05-28 11:52:12 +0100

    Only show global stats for admins

--- ukrdc_fastapi/routers/api/dashboard.py
+++ ukrdc_fastapi/routers/api/dashboard.py
@@ -11,7 +11,7 @@ from ukrdc_sqla.empi import MasterRecord, WorkItem
 
 from ukrdc_fastapi.config import settings
 from ukrdc_fastapi.dependencies import get_jtrace, get_redis
-from ukrdc_fastapi.dependencies.auth import Permissions, auth
+from ukrdc_fastapi.dependencies.auth import Permissions, UKRDCUser, auth
 from ukrdc_fastapi.utils.statistics import TotalDayPrev, total_day_prev
 
 router = APIRouter(tags=["Summary Dashboard"])
@@ -43,47 +43,53 @@ class DashboardSchema(BaseModel):
     ukrdcrecords: Optional[UKRDCRecordsDashSchema] = None
 
 
-@router.get(
-    "/",
-    response_model=DashboardSchema,
-    dependencies=[Security(auth.permission(Permissions.READ_WORKITEMS))],
-)
-def dashboard(
-    refresh: bool = False,
-    jtrace: Session = Depends(get_jtrace),
-    redis: Redis = Depends(get_redis),
-):
-    """Retreive basic statistics about recent records"""
-    # TODO: Make this an administrators dashboard. Separate stats for individual units
-    dash = DashboardSchema(messages=settings.motd, warnings=settings.wotd)
-
-    # Workitem stats
+def _get_workitems_stats(jtrace: Session, redis: Session, refresh: bool = False):
     if redis.exists("dashboard:workitems") and not refresh:
-        dash.workitems = WorkItemsDashSchema(**redis.hgetall("dashboard:workitems"))
+        return WorkItemsDashSchema(**redis.hgetall("dashboard:workitems"))
     else:
         open_workitems_stats: TotalDayPrev = total_day_prev(
             jtrace.query(WorkItem).filter(WorkItem.status == 1),
             WorkItem,
             "last_updated",
         ).dict()
-        dash.workitems = WorkItemsDashSchema(**open_workitems_stats)
+        workitems = WorkItemsDashSchema(**open_workitems_stats)
         redis.hset("dashboard:workitems", mapping=open_workitems_stats)  # type: ignore
         # Remove cached statistics after 15 minutes. Next request will re-query
         redis.expire("dashboard:workitems", 900)
+        return workitems
 
+
+def _get_empi_stats(jtrace: Session, redis: Session, refresh: bool = False):
     if redis.exists("dashboard:ukrdcrecords") and not refresh:
-        dash.ukrdcrecords = UKRDCRecordsDashSchema(
-            **redis.hgetall("dashboard:ukrdcrecords")
-        )
+        return UKRDCRecordsDashSchema(**redis.hgetall("dashboard:ukrdcrecords"))
     else:
         ukrdc_masterrecords_stats: TotalDayPrev = total_day_prev(
             jtrace.query(MasterRecord).filter(MasterRecord.nationalid_type == "UKRDC"),
             MasterRecord,
             "creation_date",
         ).dict()
-        dash.ukrdcrecords = UKRDCRecordsDashSchema(**ukrdc_masterrecords_stats)
+        ukrdcrecords = UKRDCRecordsDashSchema(**ukrdc_masterrecords_stats)
         redis.hset("dashboard:ukrdcrecords", mapping=ukrdc_masterrecords_stats)  # type: ignore
         # Remove cached statistics after 15 minutes. Next request will re-query
         redis.expire("dashboard:ukrdcrecords", 900)
+        return ukrdcrecords
+
+
+@router.get("/", response_model=DashboardSchema)
+def dashboard(
+    refresh: bool = False,
+    jtrace: Session = Depends(get_jtrace),
+    redis: Redis = Depends(get_redis),
+    user: UKRDCUser = Security(auth.get_user),
+):
+    """Retreive basic statistics about recent records"""
+    dash = DashboardSchema(messages=settings.motd, warnings=settings.wotd)
+
+    units = Permissions.unit_codes(user.permissions)
+
+    # Admin statistics
+    if Permissions.UNIT_WILDCARD in units:
+        dash.workitems = _get_workitems_stats(jtrace, redis, refresh=refresh)
+        dash.ukrdcrecords = _get_empi_stats(jtrace, redis, refresh=refresh)
 
     return dash
