a403fbc6e8e7a20f45dff57cebf4bbc07c777ffb
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-07-05 15:55:02 +0000

    Added ni filter and error XML viewer

--- ukrdc_fastapi/routers/api/v1/errors/messages.py
+++ ukrdc_fastapi/routers/api/v1/errors/messages.py
@@ -1,17 +1,28 @@
 import datetime
 from typing import Optional
 
-from fastapi import APIRouter, Depends, Security
+from fastapi import APIRouter, Depends
+from fastapi import Query as QueryParam
+from fastapi import Security
+from fastapi.exceptions import HTTPException
+from mirth_client.mirth import MirthAPI
+from mirth_client.models import ConnectorMessageData, ConnectorMessageModel
 from sqlalchemy.orm import Session
 
-from ukrdc_fastapi.dependencies import get_errorsdb, get_jtrace
+from ukrdc_fastapi.dependencies import get_errorsdb, get_jtrace, get_mirth
 from ukrdc_fastapi.dependencies.auth import UKRDCUser, auth
 from ukrdc_fastapi.query.errors import get_error, get_errors
+from ukrdc_fastapi.schemas.base import OrmModel
 from ukrdc_fastapi.schemas.errors import MessageSchema
 from ukrdc_fastapi.utils.errors import ExtendedErrorSchema
 from ukrdc_fastapi.utils.paginate import Page, paginate
 
 router = APIRouter(tags=["Errors/Messages"])
+
+
+class MessageSourceSchema(OrmModel):
+    content: Optional[str]
+    content_type: Optional[str]
 
 
 @router.get(
@@ -24,6 +35,7 @@ def error_messages(
     since: Optional[datetime.datetime] = None,
     until: Optional[datetime.datetime] = None,
     status: str = "ERROR",
+    ni: Optional[list[str]] = QueryParam([]),
     user: UKRDCUser = Security(auth.get_user),
     errorsdb: Session = Depends(get_errorsdb),
 ):
@@ -33,7 +45,13 @@ def error_messages(
     """
     return paginate(
         get_errors(
-            errorsdb, user, status=status, facility=facility, since=since, until=until
+            errorsdb,
+            user,
+            status=status,
+            nis=ni,
+            facility=facility,
+            since=since,
+            until=until,
         )
     )
 
@@ -51,3 +69,46 @@ def error_detail(
 ):
     """Retreive detailed information about a specific error message"""
     return get_error(errorsdb, jtrace, error_id, user)
+
+
+@router.get(
+    "/{error_id}/source",
+    response_model=MessageSourceSchema,
+    dependencies=[Security(auth.permission(auth.permissions.READ_ERRORS))],
+)
+async def error_source(
+    error_id: str,
+    user: UKRDCUser = Security(auth.get_user),
+    errorsdb: Session = Depends(get_errorsdb),
+    jtrace: Session = Depends(get_jtrace),
+    mirth: MirthAPI = Depends(get_mirth),
+):
+    """Retreive detailed information about a specific error message"""
+    # TODO: Run get_errors in threadpool
+    error = get_error(errorsdb, jtrace, error_id, user)
+
+    message = await mirth.channel(error.channel_id).get_message(
+        error.message_id, include_content=True
+    )
+    if not message:
+        raise HTTPException(404, "Mirth message not found")
+
+    first_connector_message: ConnectorMessageModel = list(
+        message.connector_messages.values()
+    )[0]
+
+    message_data: Optional[ConnectorMessageData]
+
+    # Prioritise encoded message over raw
+    if first_connector_message.encoded:
+        message_data = first_connector_message.encoded
+    elif first_connector_message.raw:
+        message_data = first_connector_message.raw
+
+    # If no data is available, return a valid but empty MessageSourceSchema
+    if not message_data:
+        return MessageSourceSchema(content=None, content_type=None)
+
+    return MessageSourceSchema(
+        content=message_data.content, content_type=message_data.data_type
+    )
