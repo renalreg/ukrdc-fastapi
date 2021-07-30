f4db2c1af01ad52b4c92bb002dfe1121cdde55a6
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-07-30 10:08:47 +0000

    Simplified session context managers

--- ukrdc_fastapi/dependencies/database.py
+++ ukrdc_fastapi/dependencies/database.py
@@ -1,5 +1,9 @@
+from contextlib import contextmanager
+from typing import Generator
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 
 from ukrdc_fastapi.config import settings
 from ukrdc_fastapi.utils import build_db_uri
@@ -15,9 +19,25 @@ Ukrdc3Session = sessionmaker(
             settings.ukrdc_user,
             settings.ukrdc_pass,
             settings.ukrdc_name,
-        )
+        ),
+        connect_args={"application_name": "ukrdc-fastapi"},
     ),
 )
+
+
+@contextmanager
+def ukrdc3_session() -> Generator[Session, None, None]:
+    """Yeild a new UKRDC3 database session
+
+    Yields:
+        [Session]: UKRDC3 database session
+    """
+    session = Ukrdc3Session()
+    try:
+        yield session
+    finally:
+        session.close()
+
 
 JtraceSession = sessionmaker(
     autocommit=False,
@@ -30,9 +50,25 @@ JtraceSession = sessionmaker(
             settings.jtrace_user,
             settings.jtrace_pass,
             settings.jtrace_name,
-        )
+        ),
+        connect_args={"application_name": "ukrdc-fastapi"},
     ),
 )
+
+
+@contextmanager
+def jtrace_session() -> Generator[Session, None, None]:
+    """Yeild a new JTRACE database session
+
+    Yields:
+        [Session]: JTRACE database session
+    """
+    session = JtraceSession()
+    try:
+        yield session
+    finally:
+        session.close()
+
 
 ErrorsSession = sessionmaker(
     autocommit=False,
@@ -45,6 +81,21 @@ ErrorsSession = sessionmaker(
             settings.errors_user,
             settings.errors_pass,
             settings.errors_name,
-        )
+        ),
+        connect_args={"application_name": "ukrdc-fastapi"},
     ),
 )
+
+
+@contextmanager
+def errors_session() -> Generator[Session, None, None]:
+    """Yeild a new ERRORSDB database session
+
+    Yields:
+        [Session]: ERRORSDB database session
+    """
+    session = ErrorsSession()
+    try:
+        yield session
+    finally:
+        session.close()
--- ukrdc_fastapi/tasks.py
+++ ukrdc_fastapi/tasks.py
@@ -1,16 +1,16 @@
 import logging
 
 from fastapi_utils.tasks import repeat_every
-from mirth_client.mirth import MirthAPI
 from ukrdc_sqla.ukrdc import Code
 
 from ukrdc_fastapi.config import settings
 from ukrdc_fastapi.dependencies import get_redis
 from ukrdc_fastapi.dependencies.database import (
-    ErrorsSession,
-    JtraceSession,
-    Ukrdc3Session,
+    errors_session,
+    jtrace_session,
+    ukrdc3_session,
 )
+from ukrdc_fastapi.dependencies.mirth import mirth_session
 from ukrdc_fastapi.query.dashboard import get_empi_stats, get_workitems_stats
 from ukrdc_fastapi.query.facilities import (
     _get_and_cache_errors_history,
@@ -26,36 +26,30 @@ from ukrdc_fastapi.utils.mirth import (
 @repeat_every(seconds=settings.cache_statistics_seconds)
 def cache_all_facilities() -> None:
     """FastAPI Utils task to refresh statistics for each facility"""
-    ukrdc3 = Ukrdc3Session()
-    errorsdb = ErrorsSession()
-    redis = get_redis()
-    logging.info("Refreshing facility statistics")
-    codes = ukrdc3.query(Code).filter(Code.coding_standard == "RR1+").all()
-    for code in codes:
-        logging.debug("Caching %s", code.code)
-        _get_and_cache_facility(code, ukrdc3, errorsdb, redis)
-        _get_and_cache_errors_history(code, errorsdb, redis)
-    ukrdc3.close()
-    errorsdb.close()
+    with ukrdc3_session() as ukrdc3, errors_session() as errorsdb:
+        redis = get_redis()
+        logging.info("Refreshing facility statistics")
+        codes = ukrdc3.query(Code).filter(Code.coding_standard == "RR1+").all()
+        for code in codes:
+            logging.debug("Caching %s", code.code)
+            _get_and_cache_facility(code, ukrdc3, errorsdb, redis)
+            _get_and_cache_errors_history(code, errorsdb, redis)
 
 
 @repeat_every(seconds=settings.cache_dashboard_seconds)
 def cache_dash_stats() -> None:
     """FastAPI Utils task to refresh statistics for the admin dashboard"""
-    jtrace = JtraceSession()
-    redis = get_redis()
-    logging.info("Refreshing admin statistics")
-    get_workitems_stats(jtrace, redis, refresh=True)
-    get_empi_stats(jtrace, redis, refresh=True)
-    jtrace.close()
+    with jtrace_session() as jtrace:
+        redis = get_redis()
+        logging.info("Refreshing admin statistics")
+        get_workitems_stats(jtrace, redis, refresh=True)
+        get_empi_stats(jtrace, redis, refresh=True)
 
 
 @repeat_every(seconds=settings.cache_channel_seconds)
 async def cache_mirth_channel_info() -> None:
     """FastAPI Utils task to refresh Mirth channel info"""
-    async with MirthAPI(
-        settings.mirth_url, verify_ssl=settings.mirth_verify_ssl, timeout=None
-    ) as mirth:
+    async with mirth_session() as mirth:
         await mirth.login(settings.mirth_user, settings.mirth_pass)
         redis = get_redis()
         logging.info("Refreshing Mirth channel infos")
@@ -65,9 +59,7 @@ async def cache_mirth_channel_info() -> None:
 @repeat_every(seconds=settings.cache_groups_seconds)
 async def cache_mirth_channel_groups() -> None:
     """FastAPI Utils task to refresh Mirth channel groups"""
-    async with MirthAPI(
-        settings.mirth_url, verify_ssl=settings.mirth_verify_ssl, timeout=None
-    ) as mirth:
+    async with mirth_session() as mirth:
         await mirth.login(settings.mirth_user, settings.mirth_pass)
         redis = get_redis()
         logging.info("Refreshing Mirth channel groups")
@@ -77,9 +69,7 @@ async def cache_mirth_channel_groups() -> None:
 @repeat_every(seconds=settings.cache_statistics_seconds)
 async def cache_mirth_channel_statistics() -> None:
     """FastAPI Utils task to refresh Mirth channel statistics"""
-    async with MirthAPI(
-        settings.mirth_url, verify_ssl=settings.mirth_verify_ssl, timeout=None
-    ) as mirth:
+    async with mirth_session() as mirth:
         await mirth.login(settings.mirth_user, settings.mirth_pass)
         redis = get_redis()
         logging.info("Refreshing Mirth channel statistics")
