35e22ed394aaa1056ae1170d161e1607d90ae2f4
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-08-17 10:23:19 +0000

    Updated test to new Treatment schema

--- tests/test_routers/test_patientrecord/test_treatments.py
+++ tests/test_routers/test_patientrecord/test_treatments.py
@@ -1,25 +1,30 @@
 def test_record_treatments(client):
     response = client.get("/api/v1/patientrecords/PYTEST01:PV:00000000A/treatments")
     assert response.status_code == 200
+    print(response.json())
     assert response.json() == [
         {
             "id": "TREATMENT1",
-            "pid": "PYTEST01:PV:00000000A",
             "fromTime": "2019-03-16",
             "toTime": None,
             "admitReasonCode": "1",
-            "admissionSourceCodeStd": "CF_RR7_TREATMENT",
+            "admitReasonCodeStd": None,
+            "admitReasonDesc": None,
+            "dischargeReasonCode": None,
+            "dischargeReasonCodeStd": None,
+            "dischargeReasonDesc": None,
             "healthCareFacilityCode": "TEST_SENDING_FACILITY_1",
-            "healthCareFacilityCodeStd": "ODS",
         },
         {
             "id": "TREATMENT2",
-            "pid": "PYTEST01:PV:00000000A",
             "fromTime": "2019-03-16",
             "toTime": "9999-03-16",
             "admitReasonCode": "1",
-            "admissionSourceCodeStd": "CF_RR7_TREATMENT",
+            "admitReasonCodeStd": None,
+            "admitReasonDesc": None,
+            "dischargeReasonCode": None,
+            "dischargeReasonCodeStd": None,
+            "dischargeReasonDesc": None,
             "healthCareFacilityCode": "TEST_SENDING_FACILITY_1",
-            "healthCareFacilityCodeStd": "ODS",
         },
     ]
