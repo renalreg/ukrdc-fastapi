083726a2dcdb65495390a3092bd4e0e4377d16c1
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-08-17 10:39:54 +0000

    Remove print statement

--- tests/test_routers/test_patientrecord/test_treatments.py
+++ tests/test_routers/test_patientrecord/test_treatments.py
@@ -1,7 +1,6 @@
 def test_record_treatments(client):
     response = client.get("/api/v1/patientrecords/PYTEST01:PV:00000000A/treatments")
     assert response.status_code == 200
-    print(response.json())
     assert response.json() == [
         {
             "id": "TREATMENT1",
