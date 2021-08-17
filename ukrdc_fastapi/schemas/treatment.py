c4ce8dff11c506e1045415cc41d942ee115c4468
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-08-17 09:53:55 +0000

    Added additional code lookup fields

--- ukrdc_fastapi/schemas/treatment.py
+++ ukrdc_fastapi/schemas/treatment.py
@@ -6,13 +6,16 @@ from .base import OrmModel
 
 class TreatmentSchema(OrmModel):
     id: str
-    pid: str
 
     from_time: Optional[datetime.date]
     to_time: Optional[datetime.date]
 
     admit_reason_code: Optional[str]
-    admission_source_code_std: Optional[str]
+    admit_reason_code_std: Optional[str]
+    admit_reason_desc: Optional[str]
+
+    discharge_reason_code: Optional[str]
+    discharge_reason_code_std: Optional[str]
+    discharge_reason_desc: Optional[str]
 
     health_care_facility_code: Optional[str]
-    health_care_facility_code_std: Optional[str]
