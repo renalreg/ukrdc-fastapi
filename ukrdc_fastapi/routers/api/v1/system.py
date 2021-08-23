50a4b362c7447c5b25f2ce8357ba29426b97e28f
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-08-23 11:55:06 +0000

    Fixed paths and schema keys

--- ukrdc_fastapi/routers/api/v1/system.py
+++ ukrdc_fastapi/routers/api/v1/system.py
@@ -2,33 +2,33 @@ import os
 from typing import Optional
 
 from fastapi import APIRouter, Security
-from pydantic import BaseModel
 
 from ukrdc_fastapi.config import settings
 from ukrdc_fastapi.dependencies.auth import UKRDCUser, auth
+from ukrdc_fastapi.schemas.base import JSONModel
 
 router = APIRouter(tags=["System Info"])
 
 
-class UserSchema(BaseModel):
+class UserSchema(JSONModel):
     permissions: Optional[list[str]]
     email: Optional[str]
 
 
-class SystemInfoSchema(BaseModel):
-    github_SHA: str = os.getenv("GITHUB_SHA", "Not Available")
-    github_REF: str = os.getenv("GITHUB_REF", "Not Available")
+class SystemInfoSchema(JSONModel):
+    github_sha: str = os.getenv("GITHUB_SHA", "Not Available")
+    github_ref: str = os.getenv("GITHUB_REF", "Not Available")
     deployment_env: str = settings.deployment_env
 
 
-@router.get("/user", response_model=UserSchema)
+@router.get("/user/", response_model=UserSchema)
 def system_user(user: UKRDCUser = Security(auth.get_user)):
     """Retreive basic user info"""
     return UserSchema(email=user.email, permissions=user.permissions)
 
 
 @router.get(
-    "/info", response_model=SystemInfoSchema, dependencies=[Security(auth.get_user)]
+    "/info/", response_model=SystemInfoSchema, dependencies=[Security(auth.get_user)]
 )
 def system_info():
     """Retreive basic system info"""
