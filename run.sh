81c77e49bdca2ecacaaf0bb4a48dc55d505d7a27
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-09-17 12:01:39 +0000

    Enable system root certs for local development

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 export WOTD='["Running on internal debug server. Not for deployment."]'
 poetry run uvicorn ukrdc_fastapi.main:app --host 0.0.0.0
