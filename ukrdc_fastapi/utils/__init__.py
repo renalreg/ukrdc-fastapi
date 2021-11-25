70e4687e82723b6dca29087be7cb1b7e625a3e0e
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-11-25 15:30:38 +0000

    Added reusable timer class

--- ukrdc_fastapi/utils/__init__.py
+++ ukrdc_fastapi/utils/__init__.py
@@ -1,5 +1,18 @@
 import datetime
+from time import time
 from typing import Optional
+
+
+class Timer(object):
+    def __init__(self, description):
+        self.description = description
+
+    def __enter__(self):
+        self.start = time()
+
+    def __exit__(self, type, value, traceback):
+        self.end = time()
+        print(f"{self.description}: {self.end - self.start}")
 
 
 def build_db_uri(
