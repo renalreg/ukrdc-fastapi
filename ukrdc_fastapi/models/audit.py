aafee5c67425dd86b83c9098238c7829d809d31a
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-12-06 15:36:47 +0000

    Added relationships to audit models

--- ukrdc_fastapi/models/audit.py
+++ ukrdc_fastapi/models/audit.py
@@ -1,5 +1,8 @@
+from typing import List
+
 from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.sql.schema import ForeignKey
 
 Base = declarative_base()
@@ -21,12 +24,27 @@ class AccessEvent(Base):
     method = Column(String)  # API method
     body = Column(String)  # API request body
 
+    patient_record_events: Mapped[List["PatientRecordEvent"]] = relationship(
+        "PatientRecordEvent", back_populates="access_event"
+    )
+    master_record_events: Mapped[List["MasterRecordEvent"]] = relationship(
+        "MasterRecordEvent", back_populates="access_event"
+    )
+    person_events: Mapped[List["PersonEvent"]] = relationship(
+        "PersonEvent", back_populates="access_event"
+    )
+    message_events: Mapped[List["MessageEvent"]] = relationship(
+        "MessageEvent", back_populates="access_event"
+    )
+
 
 class PatientRecordEvent(Base):
     __tablename__ = "patient_record_event"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+
     event = Column(Integer, ForeignKey("access_event.event"))
+    access_event = relationship("AccessEvent", back_populates="patient_record_events")
 
     pid = Column(String, nullable=False)  # Patient ID
 
@@ -41,7 +59,9 @@ class MasterRecordEvent(Base):
     __tablename__ = "master_record_event"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+
     event = Column(Integer, ForeignKey("access_event.event"))
+    access_event = relationship("AccessEvent", back_populates="master_record_events")
 
     master_id = Column(Integer, nullable=False)  # Master Record ID
 
@@ -53,7 +73,9 @@ class PersonEvent(Base):
     __tablename__ = "person_event"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+
     event = Column(Integer, ForeignKey("access_event.event"))
+    access_event = relationship("AccessEvent", back_populates="person_events")
 
     person_id = Column(Integer, nullable=False)  # Person ID
 
@@ -65,7 +87,9 @@ class MessageEvent(Base):
     __tablename__ = "message_event"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+
     event = Column(Integer, ForeignKey("access_event.event"))
+    access_event = relationship("AccessEvent", back_populates="message_events")
 
     message_id = Column(Integer, nullable=False)  # Message ID
 
