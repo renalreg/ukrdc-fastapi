2d895f2fd299fceab62d25df5bc32359c1de6880
Joel Collins <joel.collins@renalregistry.nhs.uk>
2021-12-08 16:00:52 +0000

    Fixed permissions test

--- tests/test_routers/test_system.py
+++ tests/test_routers/test_system.py
@@ -1,29 +1,13 @@
 import pytest
 
+from ukrdc_fastapi.dependencies.auth import Permissions
 from ukrdc_fastapi.routers.api.v1 import system
 
 
 def test_user(client):
     response = client.get("/api/v1/system/user/")
     assert response.json().get("email") == "TEST@UKRDC_FASTAPI"
-    assert set(response.json().get("permissions")) == set(
-        [
-            "ukrdc:records:read",
-            "ukrdc:messages:read",
-            "ukrdc:mirth:read",
-            "ukrdc:workitems:read",
-            "ukrdc:codes:read",
-            "ukrdc:records:write",
-            "ukrdc:messages:write",
-            "ukrdc:mirth:write",
-            "ukrdc:workitems:write",
-            "ukrdc:codes:write",
-            "ukrdc:records:export",
-            "ukrdc:records:delete",
-            "ukrdc:empi:write",
-            "ukrdc:unit:*",
-        ]
-    )
+    assert set(response.json().get("permissions")) == set(Permissions.all())
 
 
 def test_info(client):
