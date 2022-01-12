4929d2574ae9dcc54d2ce116b50020950cddc83f
Joel Collins <me@jtcollins.net>
2022-01-12 09:54:06 +0000

    Change base URL to a fixed/build-time value

--- ukrdc_fastapi/main.py
+++ ukrdc_fastapi/main.py
@@ -26,10 +26,10 @@ app = FastAPI(
     title="UKRDC API",
     description="Early test version of an updated, simpler UKRDC API",
     version=configuration.version,
-    openapi_url=f"{settings.api_base.rstrip('/')}/openapi.json",
-    docs_url=f"{settings.api_base.rstrip('/')}/docs",
-    redoc_url=f"{settings.api_base.rstrip('/')}/redoc",
-    swagger_ui_oauth2_redirect_url=f"{settings.api_base.rstrip('/')}/docs/oauth2-redirect",
+    openapi_url=f"{configuration.base_url.rstrip('/')}/openapi.json",
+    docs_url=f"{configuration.base_url.rstrip('/')}/docs",
+    redoc_url=f"{configuration.base_url.rstrip('/')}/redoc",
+    swagger_ui_oauth2_redirect_url=f"{configuration.base_url.rstrip('/')}/docs/oauth2-redirect",
     swagger_ui_init_oauth={
         "usePkceWithAuthorizationCodeGrant": True,
         "clientId": settings.swagger_client_id,
@@ -41,7 +41,7 @@ app = FastAPI(
 
 app.include_router(
     api.router,
-    prefix=settings.api_base,
+    prefix=configuration.base_url,
     dependencies=[Depends(auth.okta_jwt_scheme)],
 )
 
