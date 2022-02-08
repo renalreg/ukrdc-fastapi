be4fc59117b624c4271fcfa8eb3f7003ccba235f
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-02-08 14:34:10 +0000

    Add membership query tests

---
+++ tests/test_query/test_memberships.py
@@ -0,0 +1,28 @@
+from ukrdc_sqla.ukrdc import PatientRecord
+
+from ukrdc_fastapi.query.memberships import (
+    get_active_memberships_for_patientrecord,
+    record_has_active_membership,
+)
+
+
+def test_get_active_memberships_for_patientrecord(ukrdc3_session):
+    record = ukrdc3_session.query(PatientRecord).get("PYTEST01:PV:00000000A")
+    memberships = get_active_memberships_for_patientrecord(ukrdc3_session, record).all()
+    assert len(memberships) == 1
+    assert memberships[0].program_name == "PROGRAM_NAME_1"
+
+
+def test_record_has_active_membership_exists_active(ukrdc3_session):
+    record = ukrdc3_session.query(PatientRecord).get("PYTEST01:PV:00000000A")
+    assert record_has_active_membership(ukrdc3_session, record, "PROGRAM_NAME_1")
+
+
+def test_record_has_active_membership_exists_inactive(ukrdc3_session):
+    record = ukrdc3_session.query(PatientRecord).get("PYTEST01:PV:00000000A")
+    assert not record_has_active_membership(ukrdc3_session, record, "PROGRAM_NAME_2")
+
+
+def test_record_has_active_membership_notexists(ukrdc3_session):
+    record = ukrdc3_session.query(PatientRecord).get("PYTEST01:PV:00000000A")
+    assert not record_has_active_membership(ukrdc3_session, record, "PROGRAM_NAME_X")
