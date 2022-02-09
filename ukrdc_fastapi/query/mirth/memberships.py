7316f46a258348560c523ba173f53474e81ca6cb
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-02-09 15:52:07 +0000

    Pass master record directly into create_pkb_membership

--- ukrdc_fastapi/query/mirth/memberships.py
+++ ukrdc_fastapi/query/mirth/memberships.py
@@ -1,24 +1,21 @@
 from mirth_client.mirth import MirthAPI
 from redis import Redis
-from sqlalchemy.orm import Session
 from ukrdc_sqla.empi import MasterRecord
 
-from ukrdc_fastapi.dependencies.auth import UKRDCUser
-from ukrdc_fastapi.query.masterrecords import get_masterrecord_from_ukrdcid
+from ukrdc_fastapi.exceptions import RecordTypeError
 from ukrdc_fastapi.query.mirth.base import safe_send_mirth_message_to_name
 from ukrdc_fastapi.utils.mirth import MirthMessageResponseSchema
 from ukrdc_fastapi.utils.mirth.messages.pkb import build_pkb_membership_message
 
 
 async def create_pkb_membership(
-    ukrdcid: str,
-    user: UKRDCUser,
-    jtrace: Session,
+    master_record: MasterRecord,
     mirth: MirthAPI,
     redis: Redis,
 ) -> MirthMessageResponseSchema:
     """Export a specific patient's data to PV"""
-    master_record: MasterRecord = get_masterrecord_from_ukrdcid(jtrace, ukrdcid, user)
+    if master_record.nationalid_type != "UKRDC":
+        raise RecordTypeError("Cannot create PKB membership from a non-UKRDC record")
     return await safe_send_mirth_message_to_name(
         "PKB - New Patients",
         build_pkb_membership_message(master_record.nationalid),
