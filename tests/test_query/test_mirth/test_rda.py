d7eeacb02e0ca19b921c56b6926156cb74bb144f
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-02-10 11:05:52 +0000

    Add demographic update query logic

---
+++ tests/test_query/test_mirth/test_rda.py
@@ -0,0 +1,31 @@
+import datetime
+
+import pytest
+from ukrdc_sqla.ukrdc import PatientRecord
+
+from ukrdc_fastapi.query.mirth import rda
+from ukrdc_fastapi.schemas.patient import AddressSchema, NameSchema
+
+
+@pytest.mark.asyncio
+async def test_create_pkb_membership(ukrdc3_session, redis_session, mirth_session):
+    PID_1 = "PYTEST01:PV:00000000A"
+    record = ukrdc3_session.query(PatientRecord).get(PID_1)
+
+    response = await rda.update_patient_demographics(
+        record,
+        NameSchema(given="NEWGIVEN", family="NEWFAMILY"),
+        datetime.date(1985, 1, 1),
+        "9",
+        AddressSchema(postcode="XX0 1TT"),
+        mirth_session,
+        redis_session,
+    )
+    assert response.status == "success"
+    assert (
+        response.message
+        == """
+<?xml version="1.0" encoding="UTF-8"?>
+<ns0:PatientRecord xmlns:ns0="http://www.rixg.org.uk/"><SendingFacility>TEST_SENDING_FACILITY_1</SendingFacility><SendingExtract>PV</SendingExtract><Patient><PatientNumbers><PatientNumber><Number>888888888</Number><Organization>NHS</Organization><NumberType>NI</NumberType></PatientNumber></PatientNumbers><Names><Name use="L"><Family>NEWFAMILY</Family><Given>NEWGIVEN</Given></Name></Names><BirthTime>1985-01-01</BirthTime><Gender>9</Gender><Addresses><Address><Postcode>XX0 1TT</Postcode></Address></Addresses></Patient></ns0:PatientRecord>
+    """.strip()
+    )
---
+++ ukrdc_fastapi/query/mirth/rda.py
@@ -0,0 +1,29 @@
+import datetime
+from typing import Optional
+
+from mirth_client.mirth import MirthAPI
+from redis import Redis
+from ukrdc_sqla.ukrdc import PatientRecord
+
+from ukrdc_fastapi.query.mirth.base import safe_send_mirth_message_to_name
+from ukrdc_fastapi.schemas.patient import AddressSchema, GenderType, NameSchema
+from ukrdc_fastapi.utils.mirth import MirthMessageResponseSchema
+from ukrdc_fastapi.utils.mirth.messages.rda import build_demographic_update_message
+
+
+async def update_patient_demographics(
+    record: PatientRecord,
+    name: Optional[NameSchema],
+    dob: Optional[datetime.date],
+    gender: Optional[GenderType],
+    address: Optional[AddressSchema],
+    mirth: MirthAPI,
+    redis: Redis,
+) -> MirthMessageResponseSchema:
+    """Update the demographic data of a given patient record"""
+    return await safe_send_mirth_message_to_name(
+        "Generic RDA Inbound",
+        build_demographic_update_message(record, name, dob, gender, address),
+        mirth,
+        redis,
+    )
