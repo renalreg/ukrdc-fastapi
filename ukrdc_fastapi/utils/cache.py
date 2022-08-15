3efce90b1aaba7729457dba89938f47f9677053a
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-08-15 21:02:14 +0000

    Switch Mirth channel info caching to new cache system

--- ukrdc_fastapi/utils/cache.py
+++ ukrdc_fastapi/utils/cache.py
@@ -12,7 +12,7 @@ class CacheNotSetException(Exception):
     pass
 
 
-class ResponseCache:
+class BasicCache:
     # Create a sentinel object so we can tell the difference between no cached value,
     # and a cached None value
     _sentinel = object()
@@ -21,8 +21,6 @@ class ResponseCache:
         self,
         redis: Redis,
         key: str,
-        request: Request,
-        response: Response,
         encoder: Type[json.JSONEncoder] = JsonEncoder,
         prefix: str = "response-cache:",
     ) -> None:
@@ -30,21 +28,18 @@ class ResponseCache:
         self.encoder: Type[json.JSONEncoder] = encoder
         self.prefix = prefix
 
-        self.request: Request = request
-        self.response: Response = response
         self.key: str = key
 
-        self.etag: Optional[str] = None
-
         # Enable pre-retreiving cached value on init
-        self._cached_value: Optional[Any] = ResponseCache._sentinel
+        self._cached_value_str: Optional[str] = None
+        self._cached_value: Optional[Any] = BasicCache._sentinel
 
         # Pre-fetch the cached value
         if self.redis.exists(self.key):
             # Get the value string from Redis
             value_str = self.redis.get(self.key)
-            # Set the etag
-            self._set_etag(value_str)
+            # Store the cached value string
+            self._cached_value_str = value_str
             # Convert to JSON if the value is not None, then store the value in self._cached_value
             self._cached_value = (
                 json.loads(value_str) if value_str is not None else None
@@ -57,7 +52,65 @@ class ResponseCache:
         Returns:
             bool: Does the cached value exist
         """
-        return self._cached_value is not ResponseCache._sentinel
+        return self._cached_value is not BasicCache._sentinel
+
+    def get(self) -> Any:
+        """Get the current cached value for this key
+
+        Raises:
+            CacheNotSetException: Cached value is missing or expired, and no new value has been set.
+
+        Returns:
+            Any: Cached value
+        """
+        if self._cached_value is BasicCache._sentinel:
+            raise CacheNotSetException(
+                f"No value for key {self.key} was found in the cache. Set a value by calling cache.set(obj)."
+            )
+
+        return self._cached_value
+
+    def _dump_to_memory(self, obj: Any) -> None:
+        # Create a string representation of the object
+        value_str = json.dumps(obj, cls=self.encoder)
+        # Hold the cached value string in memory for the duration of this request
+        self._cached_value_str = value_str
+        # Hold the cached value in memory for the duration of this request
+        self._cached_value = json.loads(value_str)
+
+    def set(self, obj: Any, expire: Optional[int] = None) -> None:
+        """Set a new cached value for this key
+
+        Args:
+            obj (Any): New value to cache
+            expire (Optional[int]): Expiry time in seconds. Defaults to None.
+        """
+        # Dump the object to a string, and store in the cache class instance
+        self._dump_to_memory(obj)
+
+        # Set the value in Redis
+        if self._cached_value_str:
+            self.redis.set(self.key, self._cached_value_str, ex=expire)
+
+
+class ResponseCache(BasicCache):
+    def __init__(
+        self,
+        redis: Redis,
+        key: str,
+        request: Request,
+        response: Response,
+        encoder: Type[json.JSONEncoder] = JsonEncoder,
+        prefix: str = "response-cache:",
+    ) -> None:
+        super().__init__(redis, key, encoder, prefix)
+
+        # If we have a cached value string already set, set the etag header
+        if self._cached_value_str is not None:
+            self._set_etag(self._cached_value_str)
+
+        self.request: Request = request
+        self.response: Response = response
 
     @property
     def no_store(self) -> bool:
@@ -69,22 +122,6 @@ class ResponseCache:
         if self.request.headers.get("Cache-Control") == "no-store":
             return True
         return False
-
-    def get(self) -> Any:
-        """Get the current cached value for this key
-
-        Raises:
-            CacheNotSetException: Cached value is missing or expired, and no new value has been set.
-
-        Returns:
-            Any: Cached value
-        """
-        if self._cached_value is ResponseCache._sentinel:
-            raise CacheNotSetException(
-                f"No value for key {self.key} was found in the cache. Set a value by calling cache.set(obj)."
-            )
-
-        return self._cached_value
 
     def _set_etag(self, value_str: Optional[str]):
         """Set the etag header for a given resource string
@@ -101,16 +138,18 @@ class ResponseCache:
             obj (Any): New value to cache
             expire (Optional[int]): Expiry time in seconds. Defaults to None.
         """
-        # Create a string representation of the object
-        value_str = json.dumps(obj, cls=self.encoder)
-        # Hold the cached value in memory for the duration of this request
-        self._cached_value = json.loads(value_str)
-        # Set the etag
-        self._set_etag(value_str)
-        # If the client hasn't forbidden caching for this request
-        if not self.no_store:
-            # Set the value in Redis
-            self.redis.set(self.key, value_str, ex=expire)
+        # Dump the object to a string, and store in the cache class instance
+        self._dump_to_memory(obj)
+
+        # Set the value in Redis
+        if self._cached_value_str:
+            # Set the etag
+            self._set_etag(self._cached_value_str)
+
+            # If the client hasn't forbidden caching for this request
+            if not self.no_store:
+                # Set the value in Redis
+                self.redis.set(self.key, self._cached_value_str, ex=expire)
 
     def prepare_response(self):
         """Add cache related headers to the response object"""
