31baaf50da079bf5616d0922cf5283d7806a0af3
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-09-21 16:52:01 +0100

    Simplify observations route test

--- tests/test_routers/test_patientrecord/test_observations.py
+++ tests/test_routers/test_patientrecord/test_observations.py
@@ -1,5 +1,5 @@
-from tests.utils import days_ago
 from ukrdc_fastapi.config import configuration
+from ukrdc_fastapi.schemas.observation import ObservationSchema
 
 
 async def test_record_observations(client):
@@ -7,32 +7,5 @@ async def test_record_observations(client):
         f"{configuration.base_url}/patientrecords/PYTEST01:PV:00000000A/observations"
     )
     assert response.status_code == 200
-    assert {
-        "enteredAt": None,
-        "enteredAtDescription": None,
-        "observationDesc": "OBSERVATION_SYS_1_DESC",
-        "observationTime": days_ago(730).isoformat(),
-        "observationUnits": "OBSERVATION_SYS_1_UNITS",
-        "observationValue": "OBSERVATION_SYS_1_VALUE",
-        "prePost": None,
-    } in response.json()["items"]
-
-    assert {
-        "enteredAt": None,
-        "enteredAtDescription": None,
-        "observationDesc": "OBSERVATION_DIA_1_DESC",
-        "observationTime": days_ago(730).isoformat(),
-        "observationUnits": "OBSERVATION_DIA_1_UNITS",
-        "observationValue": "OBSERVATION_DIA_1_VALUE",
-        "prePost": None,
-    } in response.json()["items"]
-
-    assert {
-        "enteredAt": None,
-        "enteredAtDescription": None,
-        "observationDesc": "OBSERVATION_DESC",
-        "observationTime": days_ago(365).isoformat(),
-        "observationUnits": "OBSERVATION_UNITS",
-        "observationValue": "OBSERVATION_VALUE",
-        "prePost": "PRE_POST",
-    } in response.json()["items"]
+    observations = [ObservationSchema(**item) for item in response.json()["items"]]
+    assert len(observations) == 3
