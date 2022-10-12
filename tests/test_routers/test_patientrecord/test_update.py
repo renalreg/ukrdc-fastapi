b63c6ab31c9ccdc31d464e70afc5e32ecb37dd1b
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-10-12 13:58:13 +0100

    Add test_record_update_demographics_no_address test

--- tests/test_routers/test_patientrecord/test_update.py
+++ tests/test_routers/test_patientrecord/test_update.py
@@ -1,7 +1,7 @@
 from ukrdc_fastapi.config import configuration
 
 
-async def test_record_export_data(client):
+async def test_record_update_demographics(client):
     demographics = {
         "name": {
             "family": "NEWFAMILY",
@@ -29,5 +29,29 @@ async def test_record_export_data(client):
         == """
 <?xml version="1.0" encoding="UTF-8"?>
 <ns0:PatientRecord xmlns:ns0="http://www.rixg.org.uk/"><SendingFacility>TEST_SENDING_FACILITY_1</SendingFacility><SendingExtract>UKRDC</SendingExtract><Patient><PatientNumbers><PatientNumber><Number>888888888</Number><Organization>NHS</Organization><NumberType>NI</NumberType></PatientNumber></PatientNumbers><Names><Name use="L"><Family>NEWFAMILY</Family><Given>NEWGIVEN</Given></Name></Names><BirthTime>1985-01-01T00:00:00</BirthTime><Gender>9</Gender><Addresses><Address><Street>1 TEST STREET</Street><Town>TEST TOWN</Town><County>TESTFORDSHIRE</County><Postcode>XX0 1TT</Postcode><Country><CodingStandard>ISO3166-1</CodingStandard><Code>GB</Code><Description>United Kingdom</Description></Country></Address></Addresses></Patient></ns0:PatientRecord>
+    """.strip()
+    )
+
+
+async def test_record_update_demographics_no_address(client):
+    demographics = {
+        "name": {
+            "family": "NEWFAMILY",
+            "given": "NEWGIVEN",
+        },
+        "birth_time": "1985-01-01T00:00:00",
+        "gender": "9",
+    }
+    response = await client.post(
+        f"{configuration.base_url}/patientrecords/PYTEST01:PV:00000000A/update/demographics",
+        json=demographics,
+    )
+
+    assert response.status_code == 200
+    assert (
+        response.json().get("message")
+        == """
+<?xml version="1.0" encoding="UTF-8"?>
+<ns0:PatientRecord xmlns:ns0="http://www.rixg.org.uk/"><SendingFacility>TEST_SENDING_FACILITY_1</SendingFacility><SendingExtract>UKRDC</SendingExtract><Patient><PatientNumbers><PatientNumber><Number>888888888</Number><Organization>NHS</Organization><NumberType>NI</NumberType></PatientNumber></PatientNumbers><Names><Name use="L"><Family>NEWFAMILY</Family><Given>NEWGIVEN</Given></Name></Names><BirthTime>1985-01-01T00:00:00</BirthTime><Gender>9</Gender></Patient></ns0:PatientRecord>
     """.strip()
     )
