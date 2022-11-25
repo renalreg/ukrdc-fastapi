2d9e2a49512acf535cf6313c0825d3878da372a0
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-11-25 11:48:09 +0000

    Added dialysis stats query and route

--- ukrdc_fastapi/query/facilities/stats.py
+++ ukrdc_fastapi/query/facilities/stats.py
@@ -1,3 +1,6 @@
+import datetime
+from typing import Optional
+
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
 from ukrdc_sqla.ukrdc import Facility
@@ -5,6 +8,7 @@ from ukrdc_stats.calculators.demographics import (
     DemographicsStats,
     DemographicStatsCalculator,
 )
+from ukrdc_stats.calculators.dialysis import DialysisStats, DialysisStatsCalculator
 
 from ukrdc_fastapi.dependencies.auth import UKRDCUser
 
@@ -37,3 +41,41 @@ def get_facility_demographic_stats(
 
     # Calculate all demographic stats
     return DemographicStatsCalculator(ukrdc3, facility.code).extract_stats()
+
+
+def get_facility_dialysis_stats(
+    ukrdc3: Session,
+    facility_code: str,
+    user: UKRDCUser,
+    since: Optional[datetime.datetime] = None,
+    until: Optional[datetime.datetime] = None,
+) -> DialysisStats:
+    """Extract dialysis statistics for all UKRDC/RDA records in a given facility
+
+    Args:
+        ukrdc3 (Session): SQLAlchemy session
+        facility_code (str): Facility/unit code
+        user (UKRDCUser): Logged-in user
+
+    Returns:
+        DialysisStats: Facility demographic distribution statistics
+    """
+    # Assert the facility exists
+    facility = ukrdc3.query(Facility).filter(Facility.code == facility_code).first()
+
+    if not facility:
+        raise HTTPException(404, detail="Facility not found")
+
+    # Assert permissions
+    _assert_permission(facility, user)
+
+    # Handle default arguments
+    from_time: datetime.datetime = (
+        since or datetime.datetime.now() - datetime.timedelta(days=365)
+    )
+    to_time: datetime.datetime = until or datetime.datetime.now()
+
+    # Calculate all demographic stats
+    return DialysisStatsCalculator(
+        ukrdc3, facility.code, from_time=from_time, to_time=to_time
+    ).extract_stats()
