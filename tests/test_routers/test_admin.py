8b5e02ccefa36f999ecde8822ccb5d1206bfa68b
Joel Collins <joel.collins@renalregistry.nhs.uk>
2023-02-16 15:28:31 +0000

    Repair router admin tests

--- tests/test_routers/test_admin.py
+++ tests/test_routers/test_admin.py
@@ -16,6 +16,13 @@ async def test_full_workitem_history(client_superuser):
     assert len(nonzero_points) > 0
 
 
+async def test_full_workitem_history_denied(client_authenticated):
+    response = await client_authenticated.get(
+        f"{configuration.base_url}/admin/workitems_history"
+    )
+    assert response.status_code == 403
+
+
 async def test_full_errors_history(client_superuser):
     response = await client_superuser.get(
         f"{configuration.base_url}/admin/errors_history"
@@ -27,6 +34,13 @@ async def test_full_errors_history(client_superuser):
     assert len(nonzero_points) > 0
 
 
+async def test_full_errors_history_denied(client_authenticated):
+    response = await client_authenticated.get(
+        f"{configuration.base_url}/admin/errors_history"
+    )
+    assert response.status_code == 403
+
+
 async def test_admin_counts(client_superuser):
     response = await client_superuser.get(f"{configuration.base_url}/admin/counts")
 
@@ -34,6 +48,11 @@ async def test_admin_counts(client_superuser):
     assert counts.distinct_patients == 4
     assert counts.open_workitems == 3
     assert counts.patients_receiving_errors == 2
+
+
+async def test_admin_counts_denied(client_authenticated):
+    response = await client_authenticated.get(f"{configuration.base_url}/admin/counts")
+    assert response.status_code == 403
 
 
 async def test_datahealth_multiple_ukrdcids(client_superuser):
@@ -51,6 +70,13 @@ async def test_datahealth_multiple_ukrdcids(client_superuser):
     } == {1, 4}
 
 
+async def test_datahealth_multiple_ukrdcids_denied(client_authenticated):
+    response = await client_authenticated.get(
+        f"{configuration.base_url}/admin/datahealth/multiple_ukrdcids"
+    )
+    assert response.status_code == 403
+
+
 async def test_record_workitem_counts(client_superuser):
     response = await client_superuser.get(
         f"{configuration.base_url}/admin/datahealth/record_workitem_counts"
@@ -62,3 +88,10 @@ async def test_record_workitem_counts(client_superuser):
 
     assert items[0].master_record.id == 4
     assert items[0].work_item_count == 2
+
+
+async def test_record_workitem_counts_denied(client_authenticated):
+    response = await client_authenticated.get(
+        f"{configuration.base_url}/admin/datahealth/record_workitem_counts"
+    )
+    assert response.status_code == 403
