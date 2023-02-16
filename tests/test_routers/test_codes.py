72121facb34a5a0270a0c1504307724f5e44e2ff
Joel Collins <joel.collins@renalregistry.nhs.uk>
2023-02-16 15:29:07 +0000

    Use basic user for codes route tests

--- tests/test_routers/test_codes.py
+++ tests/test_routers/test_codes.py
@@ -1,8 +1,8 @@
 from ukrdc_fastapi.config import configuration
 
 
-async def test_codes_list(client_superuser):
-    response = await client_superuser.get(f"{configuration.base_url}/codes/list")
+async def test_codes_list(client_authenticated):
+    response = await client_authenticated.get(f"{configuration.base_url}/codes/list")
     ids = {item.get("code") for item in response.json().get("items")}
     assert ids == {
         "TEST_SENDING_FACILITY_1",
@@ -14,16 +14,16 @@ async def test_codes_list(client_superuser):
     }
 
 
-async def test_codes_list_filter_standard(client_superuser):
-    response = await client_superuser.get(
+async def test_codes_list_filter_standard(client_authenticated):
+    response = await client_authenticated.get(
         f"{configuration.base_url}/codes/list?coding_standard=CODING_STANDARD_1"
     )
     ids = {item.get("code") for item in response.json().get("items")}
     assert ids == {"CODE_1"}
 
 
-async def test_codes_list_search(client_superuser):
-    response = await client_superuser.get(
+async def test_codes_list_search(client_authenticated):
+    response = await client_authenticated.get(
         f"{configuration.base_url}/codes/list?search=CODE_"
     )
     ids = {item.get("code") for item in response.json().get("items")}
@@ -34,31 +34,35 @@ async def test_codes_list_search(client_superuser):
     }
 
 
-async def test_code_list_export(client_superuser):
-    response = await client_superuser.get(f"{configuration.base_url}/codes/export/list")
+async def test_code_list_export(client_authenticated):
+    response = await client_authenticated.get(
+        f"{configuration.base_url}/codes/export/list"
+    )
     assert (
         response.content
         == b'"RR1+","TEST_SENDING_FACILITY_1","TEST_SENDING_FACILITY_1_DESCRIPTION"\r\n"RR1+","TEST_SENDING_FACILITY_2","TEST_SENDING_FACILITY_2_DESCRIPTION"\r\n"CODING_STANDARD_1","CODE_1","DESCRIPTION_1"\r\n"CODING_STANDARD_2","CODE_2","DESCRIPTION_2"\r\n"CODING_STANDARD_2","CODE_3","DESCRIPTION_3"\r\n"NHS_DATA_DICTIONARY","G","ETHNICITY_GROUP_CODE_DESCRIPTION"\r\n'
     )
 
 
-async def test_code_list_export_filter_standard(client_superuser):
-    response = await client_superuser.get(
+async def test_code_list_export_filter_standard(client_authenticated):
+    response = await client_authenticated.get(
         f"{configuration.base_url}/codes/export/list?coding_standard=CODING_STANDARD_1"
     )
     assert response.content == b'"CODING_STANDARD_1","CODE_1","DESCRIPTION_1"\r\n'
 
 
-async def test_code_maps_export(client_superuser):
-    response = await client_superuser.get(f"{configuration.base_url}/codes/export/maps")
+async def test_code_maps_export(client_authenticated):
+    response = await client_authenticated.get(
+        f"{configuration.base_url}/codes/export/maps"
+    )
     assert (
         response.content
         == b'"CODING_STANDARD_1","CODE_1","CODING_STANDARD_2","CODE_2"\r\n"CODING_STANDARD_2","CODE_2","CODING_STANDARD_1","CODE_1"\r\n'
     )
 
 
-async def test_code_maps_export_filter_standard(client_superuser):
-    response = await client_superuser.get(
+async def test_code_maps_export_filter_standard(client_authenticated):
+    response = await client_authenticated.get(
         f"{configuration.base_url}/codes/export/maps?source_coding_standard=CODING_STANDARD_1"
     )
     assert (
@@ -67,8 +71,8 @@ async def test_code_maps_export_filter_standard(client_superuser):
     )
 
 
-async def test_code_exclusions_export(client_superuser):
-    response = await client_superuser.get(
+async def test_code_exclusions_export(client_authenticated):
+    response = await client_authenticated.get(
         f"{configuration.base_url}/codes/export/exclusions"
     )
     assert (
@@ -77,8 +81,8 @@ async def test_code_exclusions_export(client_superuser):
     )
 
 
-async def test_code_exclusions_export_filter_standard(client_superuser):
-    response = await client_superuser.get(
+async def test_code_exclusions_export_filter_standard(client_authenticated):
+    response = await client_authenticated.get(
         f"{configuration.base_url}/codes/export/exclusions?coding_standard=CODING_STANDARD_1"
     )
     assert response.content == b'"CODING_STANDARD_1","CODE_1","SYSTEM_1"\r\n'
