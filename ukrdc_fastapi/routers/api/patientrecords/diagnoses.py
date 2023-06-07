cb5771d44f8cdf4c8f484a3926607236b1043516
Joel Collins <joel.collins@renalregistry.nhs.uk>
2023-06-07 14:57:06 +0100

    Refactor diagnoses queries into query submodule

--- ukrdc_fastapi/routers/api/patientrecords/diagnoses.py
+++ ukrdc_fastapi/routers/api/patientrecords/diagnoses.py
@@ -1,19 +1,15 @@
-from typing import Optional
-from pydantic import Field
-from sqlalchemy import and_
-from ukrdc_fastapi.schemas.patientrecord import (
-    DiagnosisSchema,
-    RenalDiagnosisSchema,
-    CauseOfDeathSchema,
+from ukrdc_fastapi.query.patientrecords.diagnoses import (
+    ExtendedCauseOfDeathSchema,
+    ExtendedDiagnosisSchema,
+    ExtendedRenalDiagnosisSchema,
+    get_patient_cause_of_death,
+    get_patient_diagnosis,
+    get_patient_renal_diagnosis,
 )
 
 from fastapi import APIRouter, Depends, Security
 from ukrdc_sqla.ukrdc import (
     PatientRecord,
-    Diagnosis,
-    RenalDiagnosis,
-    CauseOfDeath,
-    Code,
 )
 
 from ukrdc_fastapi.dependencies.audit import (
@@ -30,24 +26,6 @@ router = APIRouter()
 # Extended schemas which will include descriptions obtained by looking up codes in the code_list table
 
 
-class ExtendedDiagnosisSchema(DiagnosisSchema):
-    description: Optional[str] = Field(
-        None, description="Formal coding description, if available"
-    )
-
-
-class ExtendedRenalDiagnosisSchema(RenalDiagnosisSchema):
-    description: Optional[str] = Field(
-        None, description="Formal coding description, if available"
-    )
-
-
-class ExtendedCauseOfDeathSchema(CauseOfDeathSchema):
-    description: Optional[str] = Field(
-        None, description="Formal coding description, if available"
-    )
-
-
 @router.get(
     "/diagnosis",
     response_model=list[ExtendedDiagnosisSchema],
@@ -59,25 +37,6 @@ def patient_diagnosis(
 ):
     """Retreive a specific patient's diagnoses"""
 
-    q = (
-        patient_record.diagnoses.outerjoin(
-            Code,
-            and_(
-                Code.coding_standard == Diagnosis.diagnosiscodestd,
-                Code.code == Diagnosis.diagnosiscode,
-            ),
-        )
-        .with_entities(Diagnosis, Code.description)
-        .order_by(Diagnosis.creation_date.desc())
-    )
-
-    items = []
-
-    for row in q:
-        item = ExtendedDiagnosisSchema.from_orm(row[0])
-        item.description = row[1]
-        items.append(item)
-
     audit.add_event(
         Resource.DIAGNOSES,
         None,
@@ -87,7 +46,7 @@ def patient_diagnosis(
         ),
     )
 
-    return items
+    return get_patient_diagnosis(patient_record)
 
 
 @router.get(
@@ -100,25 +59,6 @@ def patient_renal_diagnosis(
     audit: Auditer = Depends(get_auditer),
 ):
     """Retreive a specific patient's renal diagnoses"""
-    q = (
-        patient_record.renaldiagnoses.join(
-            Code,
-            and_(
-                Code.coding_standard == RenalDiagnosis.diagnosiscodestd,
-                Code.code == RenalDiagnosis.diagnosiscode,
-            ),
-        )
-        .with_entities(RenalDiagnosis, Code.description)
-        .order_by(RenalDiagnosis.creation_date.desc())
-    )
-
-    items = []
-
-    for row in q:
-        item = ExtendedRenalDiagnosisSchema.from_orm(row[0])
-        item.description = row[1]
-        items.append(item)
-
     audit.add_event(
         Resource.RENALDIAGNOSES,
         None,
@@ -128,7 +68,7 @@ def patient_renal_diagnosis(
         ),
     )
 
-    return items
+    return get_patient_renal_diagnosis(patient_record)
 
 
 @router.get(
@@ -141,24 +81,6 @@ def patient_cause_of_death(
     audit: Auditer = Depends(get_auditer),
 ):
     """Retreive a specific patient's cause of death"""
-    q = (
-        patient_record.cause_of_death.join(
-            Code,
-            and_(
-                Code.coding_standard == CauseOfDeath.diagnosiscodestd,
-                Code.code == CauseOfDeath.diagnosiscode,
-            ),
-        )
-        .with_entities(CauseOfDeath, Code.description)
-        .order_by(CauseOfDeath.creation_date.desc())
-    )
-
-    items = []
-
-    for row in q:
-        item = ExtendedCauseOfDeathSchema.from_orm(row[0])
-        item.description = row[1]
-        items.append(item)
 
     audit.add_event(
         Resource.CAUSEOFDEATH,
@@ -169,4 +91,4 @@ def patient_cause_of_death(
         ),
     )
 
-    return items
+    return get_patient_cause_of_death(patient_record)
