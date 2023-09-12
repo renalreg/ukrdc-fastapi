63aa76f7555e55170fe7be9d7ec356cb375614d5
Joel Collins <joel.collins@renalregistry.nhs.uk>
2023-09-12 11:38:17 +0100

    Add basic scripts to analyse membership creation events

---
+++ scripts/analytics/analyse_memberships.py
@@ -0,0 +1,40 @@
+import json
+import matplotlib.pyplot as plt
+
+with open("./scripts/analytics/output/uids.json", "r") as f1:
+    uid_email_map = json.load(f1)
+
+with open("./scripts/analytics/output/events.json", "r") as f2:
+    creation_events = json.load(f2)
+
+# Basic counting
+
+for uid, events in creation_events.items():
+    friendly_id = "/".join(uid_email_map.get(uid))
+    print(friendly_id)
+    print(uid)
+    print(len(events))
+
+    events.sort()
+
+    counts_per_day = {}
+
+    for event in events:
+        date = event.split("T")[0]
+        if date not in counts_per_day:
+            counts_per_day[date] = 0
+
+        counts_per_day[date] += 1
+
+    plt.figure()
+    plt.scatter(counts_per_day.keys(), counts_per_day.values())
+    plt.title(friendly_id)
+
+    plt.xlabel("Date")
+    plt.xticks(rotation=90, fontsize=10)
+
+    plt.ylabel("New memberships")
+
+    plt.savefig(f"./scripts/analytics/output/plot/{uid}.png")
+
+    plt.close()
---
+++ scripts/analytics/query_memberships.py
@@ -0,0 +1,72 @@
+import datetime
+import json
+
+from sqlalchemy import and_
+from sqlalchemy.orm import Session
+from ukrdc_fastapi.dependencies.audit import AuditOperation, Resource
+from ukrdc_fastapi.dependencies.database import AuditSession
+from ukrdc_fastapi.models.audit import AuditEvent
+
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime.datetime, datetime.date)):
+        return obj.isoformat()
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError("Type %s not serializable" % type(obj))
+
+
+session: Session = AuditSession()
+
+membership_creations = (
+    session.query(AuditEvent)
+    .filter(
+        and_(
+            AuditEvent.resource == Resource.MEMBERSHIP.value,
+            AuditEvent.operation == AuditOperation.CREATE.value,
+            AuditEvent.resource_id == "PKB",
+        )
+    )
+    .all()
+)
+
+print(len(membership_creations))
+
+creation_events: dict[str, datetime.datetime] = {}
+uid_email_map: dict[str, set[str]] = {}
+
+i = 0
+
+for event in membership_creations:
+    uid = event.access_event.uid
+
+    if uid not in creation_events:
+        creation_events[uid] = []
+    if uid not in uid_email_map:
+        uid_email_map[uid] = set()
+
+    uid_email_map[uid].add(event.access_event.sub)
+    creation_events[uid].append(event.access_event.time)
+
+    i += 1
+    if i % 100 == 0:
+        print(i)
+
+print(uid_email_map)
+print(creation_events)
+
+with open("./scripts/analytics/output/uids.json", "w") as f1:
+    json.dump(
+        uid_email_map,
+        f1,
+        default=json_serial,
+    )
+
+with open("./scripts/analytics/output/events.json", "w") as f2:
+    json.dump(
+        creation_events,
+        f2,
+        default=json_serial,
+    )
