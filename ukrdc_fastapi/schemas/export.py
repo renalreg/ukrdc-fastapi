d55248a819248a354c565507167814ad40437424
Joel Collins <joel.collins@renalregistry.nhs.uk>
2024-01-04 10:00:05 +0000

    fix: Fix default value for ExportResponseSchema.number_of_messages

--- ukrdc_fastapi/schemas/export.py
+++ ukrdc_fastapi/schemas/export.py
@@ -6,7 +6,7 @@ from .base import OrmModel
 class ExportResponseSchema(OrmModel):
     """Response to a record export call"""
 
-    status: str = Field(..., description="Response status of the message")
+    status: str = Field(..., description="Response status of the export")
     number_of_messages: int = Field(
-        None, description="Number of messages sent in the export request"
+        ..., description="Number of messages sent in the export request"
     )
