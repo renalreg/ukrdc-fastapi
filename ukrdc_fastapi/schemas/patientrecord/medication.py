c65faddaab2b96f68435e84d5d48f91d1a9cc599
Joel Collins <joel.collins@renalregistry.nhs.uk>
2024-01-23 13:54:20 +0000

    feat: add extra medication fields

--- ukrdc_fastapi/schemas/patientrecord/medication.py
+++ ukrdc_fastapi/schemas/patientrecord/medication.py
@@ -15,8 +15,27 @@ class MedicationSchema(OrmModel):
     to_time: Optional[datetime.datetime] = Field(
         None, description="Time the patient stopped taking the medication"
     )
+
     drug_product_generic: str = Field(..., description="Generic name of the medication")
+
+    doseuomcode: Optional[str] = Field(
+        None, description="Dose unit of measurement code"
+    )
+    doseuomcodestd: Optional[str] = Field(
+        None, description="Dose unit of measurement coding standard"
+    )
+    doseuomdesc: Optional[str] = Field(
+        None, description="Dose unit of measurement description"
+    )
+
+    routecode: Optional[str] = Field(None, description="Route code")
+    routecodestd: Optional[str] = Field(None, description="Route coding standard")
+    routedesc: Optional[str] = Field(None, description="Route description")
+
+    frequency: Optional[str] = Field(None, description="Medication frequency")
+
     comment: Optional[str] = Field(None, description="Comment on the medication")
+
     entering_organization_code: Optional[str] = Field(
         None, description="Code of the organization that entered the medication"
     )
