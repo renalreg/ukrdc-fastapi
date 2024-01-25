da149e553879fb877e955d9e1125f446c075b314
Joel Collins <joel.collins@renalregistry.nhs.uk>
2024-01-25 11:33:59 +0000

    feat: clear task tracker on startup rather than shutdown

--- ukrdc_fastapi/main.py
+++ ukrdc_fastapi/main.py
@@ -1,26 +1,18 @@
 import logging
 import re
 
-import redis
-import sqlalchemy
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse
 from fastapi.routing import APIRoute
 from fastapi_pagination import add_pagination
-from httpx import ConnectError
-from mirth_client import MirthAPI
-from mirth_client.models import LoginResponse
 
 from ukrdc_fastapi.config import configuration, settings
-from ukrdc_fastapi.dependencies import get_redis
 from ukrdc_fastapi.dependencies.auth import auth
-from ukrdc_fastapi.dependencies.database import ukrdc3_session
-from ukrdc_fastapi.dependencies.mirth import mirth_session
 from ukrdc_fastapi.dependencies.sentry import add_sentry
 from ukrdc_fastapi.exceptions import ResourceNotFoundError
 from ukrdc_fastapi.routers import api
-from ukrdc_fastapi.tasks import repeated, shutdown
+from ukrdc_fastapi.tasks import repeated, startup
 
 
 # Create app
@@ -83,59 +75,15 @@ async def http_exception_handler(_, exc):
     return PlainTextResponse(str(exc), status_code=404)
 
 
-# Attach event handlers
+# Run synchronous startup functions
+logging.info("Clearing task tracker and locks...")
+startup.clear_task_tracker()
 
-# Async startup functions and event handlers
+# Attach async startup event handlers
+logging.info("Starting repeated background tasks...")
 app.router.add_event_handler("startup", repeated.update_channel_id_name_map)
 app.router.add_event_handler("startup", repeated.update_facilities_list)
 app.router.add_event_handler("startup", repeated.precalculate_facility_stats_dialysis)
-
-# Async shutdown functions and event handler
-app.router.add_event_handler("shutdown", shutdown.clear_task_tracker)
-
-
-class StartupError(RuntimeError):
-    pass
-
-
-# @app.on_event("startup")
-async def check_connections():
-    """
-    Check Mirth, database, and Redis connections
-    """
-    logging.info("Checking connection to Mirth API...")
-    mirth_api: MirthAPI
-    async with mirth_session() as mirth_api:
-        try:
-            # Check we can log in to Mirth
-            login_response: LoginResponse = await mirth_api.login(
-                settings.mirth_user, settings.mirth_pass
-            )
-            logging.debug(login_response)
-            if login_response.status != "SUCCESS":
-                raise RuntimeError("Unable to authenticate with Mirth")
-        except ConnectError as e:
-            raise StartupError(
-                f"Unable to connect to Mirth API at {settings.mirth_url}. Ensure connection is properly configured."
-            ) from e
-
-    logging.info("Checking connection to UKRDC database...")
-    with ukrdc3_session() as ukrdc3:
-        try:
-            ukrdc3.connection()
-        except sqlalchemy.exc.OperationalError as e:
-            raise StartupError(
-                "Unable to connect to UKRDC database. Ensure connection is properly configured."
-            ) from e
-
-    logging.info("Checking connection to Redis database...")
-    try:
-        get_redis().ping()
-    except redis.exceptions.ConnectionError as e:
-        raise StartupError(
-            "Unable to connect to Redis instance. Ensure redis-server is running locally."
-        ) from e
-
 
 # Run app
 
