9aa92578aa8a3d23d47194de0c1f10a752477ee8
Joel Collins <joel.collins@renalregistry.nhs.uk>
2024-02-01 13:49:54 +0000

    chore: remove unused query union function

--- tests/test_utils/test_utils.py
+++ tests/test_utils/test_utils.py
@@ -1,7 +1,3 @@
-import pytest
-from ukrdc_sqla.ukrdc import PatientRecord
-
-from tests.conftest import PID_1, PID_2, PID_3, UKRDCID_1, UKRDCID_2
 from ukrdc_fastapi import utils
 
 
@@ -18,46 +14,3 @@ def test_build_db_uri_sqlite():
         == "sqlite:///dbname"
     )
 
-
-def test_query_union_one(ukrdc3_session):
-    q1 = ukrdc3_session.query(PatientRecord).filter(PatientRecord.pid == PID_1)
-
-    u = utils.query_union([q1])
-    assert {record.pid for record in u} == {PID_1}
-
-
-def test_query_union_multiple(ukrdc3_session):
-    pids_to_test = {PID_1, PID_2, PID_3}
-    qs = [
-        ukrdc3_session.query(PatientRecord).filter(PatientRecord.pid == test_pid)
-        for test_pid in pids_to_test
-    ]
-
-    u = utils.query_union(qs)
-    assert {record.pid for record in u} == pids_to_test
-
-
-def test_query_union_multiple_overlapping(ukrdc3_session):
-    pids_to_test = {PID_1, PID_2, PID_3}
-    ukrdcids_to_test = {UKRDCID_1, UKRDCID_2}
-    qs = [
-        ukrdc3_session.query(PatientRecord).filter(PatientRecord.pid == test_pid)
-        for test_pid in pids_to_test
-    ]
-
-    qs.extend(
-        [
-            ukrdc3_session.query(PatientRecord).filter(
-                PatientRecord.ukrdcid == test_ukrdcid
-            )
-            for test_ukrdcid in ukrdcids_to_test
-        ]
-    )
-
-    u = utils.query_union(qs)
-    assert {record.pid for record in u} == pids_to_test
-
-
-def test_query_union_none():
-    with pytest.raises(ValueError):
-        utils.query_union([])
