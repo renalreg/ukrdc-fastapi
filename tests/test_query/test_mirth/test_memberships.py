163f535f5a8a7056328f9e8ef13b2d010b171858
andyatterton <andrew.atterton@ukkidney.org>
2025-04-14 17:34:28 +0100

    adjustments for testing

--- tests/test_query/test_mirth/test_memberships.py
+++ tests/test_query/test_mirth/test_memberships.py
@@ -8,10 +8,10 @@ from ukrdc_fastapi.query.mirth import memberships
 async def test_create_pkb_membership(redis_session, mirth_session):
     partner = "test_partner"
     response = await memberships.create_partner_membership_for_ukrdcid(
-        UKRDCID_1, mirth_session, redis_session
+        UKRDCID_1, mirth_session, redis_session, partner
     )
     assert response.status == "success"
     assert (
         response.message
-        == f"<result><ukrdcid>{UKRDCID_1}</ukrdcid><partner>{partner}</partner></result>"
+        == f"<request><ukrdcid>{UKRDCID_1}</ukrdcid><partner>{partner}</partner></request>"
     )
