e17d5071a80ff30e90b6d2494cf03b893c8cce44
Andy Atterton <57716459+andyatterton@users.noreply.github.com>
2025-05-04 15:14:05 +0100

    Add mrc sync (#972)
    * feat: Added full sync for MRC patients
    
    Added an endpoint and supporting logic and exceptions for MRC record syncs.
    
    * mypy fixes

--- ukrdc_fastapi/routers/api/patientrecords/export.py
+++ ukrdc_fastapi/routers/api/patientrecords/export.py
@@ -18,6 +18,7 @@ from ukrdc_fastapi.query.mirth.export import (
     export_all_to_radar,
     export_docs_to_pv,
     export_tests_to_pv,
+    export_all_to_mrc,
 )
 from ukrdc_fastapi.schemas.export import ExportResponseSchema
 
@@ -135,3 +136,28 @@ async def patient_export_pkb(
     return ExportResponseSchema(
         status=combined_status, number_of_messages=len(response)
     )
+
+
+@router.post(
+    "/mrc",
+    response_model=ExportResponseSchema,
+    dependencies=[Security(auth.permission(Permissions.EXPORT_RECORDS))],
+)
+async def patient_export_mrc(
+    patient_record: PatientRecord = Depends(_get_patientrecord),
+    ukrdc3: Session = Depends(get_ukrdc3),
+    mirth: MirthAPI = Depends(get_mirth),
+    redis: Redis = Depends(get_redis),
+    audit: Auditer = Depends(get_auditer),
+):
+    """
+    Export a specific patient's data to PKB.
+    """
+
+    response = await export_all_to_mrc(patient_record, ukrdc3, mirth, redis)
+
+    audit.add_event(
+        Resource.PATIENT_RECORD, patient_record.pid, AuditOperation.EXPORT_MRC
+    )
+
+    return ExportResponseSchema(status=response.status, number_of_messages=1)
