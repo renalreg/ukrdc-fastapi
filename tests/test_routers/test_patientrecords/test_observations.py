bd4e5a2abf94a7906d97e30364adfdb98d6f334a
phil <philip.main@renalregistry.nhs.uk>
2025-10-21 09:03:42 +0100

    fixed schema to allow null observation

--- tests/test_routers/test_patientrecords/test_observations.py
+++ tests/test_routers/test_patientrecords/test_observations.py
@@ -1,3 +1,4 @@
+from ukrdc_sqla.ukrdc import Observation, PatientRecord
 from ukrdc_fastapi.config import configuration
 from ukrdc_fastapi.schemas.patientrecord.observation import ObservationSchema
 
@@ -26,3 +27,19 @@ async def test_record_observation_codes(client_superuser):
     )
     assert response.status_code == 200
     assert set(response.json()) == {"bpdia", "OBSERVATION_CODE", "bpsys"}
+
+async def test_ui_261(client_superuser, ukrdc3_session):
+    # Update to the pydantic schema to allow null values
+
+    obs = ukrdc3_session.query(PatientRecord).join(Observation).where(PatientRecord.pid == "PYTEST01:PV:00000000A").first().observations[0]
+    obs.observationvalue = None
+    ukrdc3_session.commit()
+
+    response = await client_superuser.get(
+        f"{configuration.base_url}/patientrecords/PYTEST01:PV:00000000A/observations"
+    )
+    assert response.status_code == 200
+
+    items = response.json().get("items", [])
+    assert len(items) > 0
+    assert [ObservationSchema(**x) for x in items]
