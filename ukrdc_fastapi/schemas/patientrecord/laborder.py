3e0df13632029ef51a7547fd5cbef8a5174ec753
Alice Morris <amorris66@hotmail.co.uk>
2026-08-21 13:03:11 +0100

    feat(UK-334): add interpretation_codes to ResultItemSchema

--- ukrdc_fastapi/schemas/patientrecord/laborder.py
+++ ukrdc_fastapi/schemas/patientrecord/laborder.py
@@ -40,6 +40,9 @@ class ResultItemSchema(OrmModel):
     observation_time: datetime.datetime | None = Field(
         None, description="Observation timestamp"
     )
+    interpretation_codes: str | None = Field(
+        None, description="Code(s) indicating interpretation of the result (POS, NEG, UNK)"
+    )
 
 
 class ResultItemServiceSchema(OrmModel):
